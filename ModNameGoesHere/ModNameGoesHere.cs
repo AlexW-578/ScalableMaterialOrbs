ce6c650b8770dfb9f85b4891a8e6a78d68fb2c53
AlexW-578 <66627294+AlexW-578@users.noreply.github.com>
2023-08-26 08:50:19 +0100

    Initial commit

---
+++ ModNameGoesHere/ModNameGoesHere.cs
@@ -0,0 +1,58 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+
+namespace ModNameGoesHere
+{
+    public class ModNameGoesHere : NeosMod
+    {
+        public override string Name => "ModNameGoesHere";
+        public override string Author => "username";
+        public override string Version => "0.0.1";
+        public override string Link => "https://github.com/GithubUsername/RepoName/";
+	
+	[AutoRegisterConfigKey] private static readonly ModConfigurationKey<bool> Enabled = new ModConfigurationKey<bool>("Enabled", "Enable/Disable the Mod", () => true);
+	
+        public override void OnEngineInit()
+        {
+	    Config = GetConfiguration();
+            Config.Save(true);
+            Harmony harmony = new Harmony("net.username.Template");
+            harmony.PatchAll();
+        }
+	
+		
+        [HarmonyPatch(typeof(class you want to patch), "name of method you want to patch")]
+        class ModNameGoesHerePatch
+        {
+            static bool Prefix()
+            {
+                if (!Config.GetValue(Enabled)) { return true; }
+
+                return false;
+            }
+        }
+		
+        [HarmonyPatch(typeof(class you want to patch), "name of method you want to patch")]
+        class ModNameGoesHerePatch
+        {
+            static void Postfix()
+            {
+                if (Config.GetValue(Enabled) )
+                {
+                    node.Up = __instance.Slot.ActiveUserRoot.Slot.Up;
+                }
+            }
+        }
+	
+        [HarmonyPatch(typeof(class you want to patch), "name of method you want to patch")]
+	public class Player_beeType_Patcher
+	{
+	    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+	    {
+		var code = new List<CodeInstruction>(instructions);
+		// We'll need to modify code here.
+		return code;
+            }  
+	}
+    }
+}
---
+++ ModNameGoesHere/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ModNameGoesHere")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Name")]
+[assembly: AssemblyProduct("ModNameGoesHere")]
+[assembly: AssemblyCopyright("Copyright © Name 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("47561A7C-DA9A-4362-877E-BBFD35328A2B")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
